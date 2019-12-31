76523fc7c3e93493670738a672806437b8af63ea
VisakanMathy <vm1016@ic.ac.uk>
2019-12-31 13:29:30 +0000

    Bugfixes

--- ultra.py
+++ ultra.py
@@ -110,7 +110,7 @@ try:
 			traffic_poll = time.time()
 		todelete = []
 		for i in store.keys():
-			if store[i][2] - time.time() > 30:
+			if store[i][2] - time.time() > 120:
 				print(store[i])
 				with open('buses.csv','a',newline='') as buses_file:
 					buses_writer = csv.writer(buses_file, delimiter = ',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
